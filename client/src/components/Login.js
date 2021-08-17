6c2c26fab5b71ed0b5c3c66104d9f5cf3d75a9f7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-17 13:34:13 -0400

    lecture work-in-progress

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -28,6 +28,8 @@ class Login extends React.Component {
     axios.post('http://localhost:5000/api/login', this.state.credentials)
       .then(res => {
         localStorage.setItem("token", res.data.token);
+        localStorage.setItem("role", res.data.role);
+        localStorage.setItem("username", res.data.username);
         this.props.history.push('/protected');
       })
       .catch(err=> {

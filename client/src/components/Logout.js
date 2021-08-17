ca520f628b646c9570f36b7487329fef61efb052
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-17 13:27:26 -0400

    lecture work-in-progress

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,20 +1,16 @@
 import React, { useEffect } from "react";
-import axios from 'axios';
+import axiosWithAuth from './../utils/axiosWithAuth';
 
 const Logout = (props)=> { 
 
     useEffect(()=> {
-        const token = localStorage.getItem("token");
-
-        axios.post('http://localhost:5000/api/logout', {
-            headers: {
-                authorization: token
-            }
-        })
-        .then(res=> {
-            localStorage.removeItem("token");
-            props.history.push('/login');
-        });
+        axiosWithAuth()
+            .post('http://localhost:5000/api/logout')
+            .then(res=> {
+                localStorage.removeItem("token");
+                props.history.push('/login');
+            });
+        
     }, []);
     
     return(<div></div>);
